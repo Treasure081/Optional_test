6dbc38c73a4fc6c2126f3693d95e8fe79d8b8989
Treasure081 <tsenater@gmail.com>
2024-12-04 21:15:57 -0800

    first commit

---
+++ assignment 2.py
@@ -0,0 +1,12 @@
+numbers = [2,
+           4,
+           6,
+           8,
+           10,
+           12,
+           14,
+           16,
+           18,
+           20]
+for i in range(len(numbers)):
+    print(numbers.pop())
